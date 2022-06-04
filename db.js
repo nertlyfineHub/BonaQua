154b77012531abb579137b63fdd318951ad597cf
BonaQua <106870301+BonaQua2@users.noreply.github.com>
2022-06-04 19:14:28 +0500

    Add files via upload

---
+++ db.js
@@ -0,0 +1,22 @@
+let db = `
+
+ https://discord.gg/XdtTNszBsm
+--
+guild2.png
+--
+Kaneki Ken
+&&
+https://discord.gg/invite/NjUJajwKW7
+--
+guild3.jpg
+--
+Deepins02
+&&
+https://discord.gg/CaKr8vAz48
+--
+guild1.gif
+--
+schizophrenia
+`;
+
+export {db} 
